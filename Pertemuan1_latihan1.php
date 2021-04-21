342e5db4988510f094cd13aa693f5d5a842e43f2
unknown <fajarjul58@outlook.com>
2021-04-22 05:40:55 +0700

    Hapus

--- Pertemuan1_latihan1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$A = 123; // variable global
-function Test()
-{
-    $A = "Test"; // variable local
-    echo "Nilai A dalam fungsi = $A \n";
-}
-Test();
-echo "Nilai A luar fungsi = $A \n";
