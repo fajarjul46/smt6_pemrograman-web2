7bf5c9fb6b4d854bd8c5f4a8afd686c51a2a9dcf
unknown <fajarjul58@outlook.com>
2021-04-22 06:13:04 +0700

    Add New File

---
+++ modul4/latihan7.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Penggunaan For</title>
+</head>
+
+<body>
+    <?Php
+    for ($bil = 1; $bil < 25; $bil++)
+        print("$bil<br>\n");
+    ?>
+</body>
+
+</html>
---
+++ modul4/latihan8.php
@@ -0,0 +1,5 @@
+<?php
+for($i = 1 ;$i< 11 ;$i++) {
+if ($i % 2 == 0 ) continue ;
+else echo$i ;
+}
